e16e5772471d8538f77d09b93fd7e560ff9d86f0
Poornaravi <138842177+Poornaravi@users.noreply.github.com>
2023-07-31 16:47:24 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,7 @@
+function on() {
+    document.getElementById("1").src = "https://cdn3.vectorstock.com/i/1000x1000/89/72/object-bulb-off-vector-1858972.jpg";
+}
+
+function off() {
+    document.getElementById("1").src = "https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940319kdxsp3rf0i.png";
+}
